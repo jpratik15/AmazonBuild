473b449734b2ba9e7c65a513e9bd8468687e6de7
jpratik15 <190010034@iitdh.ac.in>
2021-05-10 17:56:41 +0530

    Add Navbar Component and Styles

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAxoq4JNAzRPVHb_NqXqwCg4zOVKu_4kSM",
+    authDomain: "build-bedd9.firebaseapp.com",
+    projectId: "build-bedd9",
+    storageBucket: "build-bedd9.appspot.com",
+    messagingSenderId: "318295483921",
+    appId: "1:318295483921:web:7ab428a3a259e5aa96f3b0",
+    measurementId: "G-EPPP604FRY"
+  };
